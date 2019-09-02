6157726d3e86ac6b16d1e5cee9bd657c1e7d9423
Bianca Gomes Rodrigues <bgsrd@outlook.com>
2019-09-01 22:02:50 -0300

    Adição da função Imprime que imprime a letra e seu respectivo número de incidência - Adição da função Imprime_Ordem_Incidencia - Adição da função Inicializa que inicializa vetores do tipo ln

--- AT1_743512.c
+++ AT1_743512.c
@@ -17,6 +17,12 @@ typedef struct {
     int Num; //Número de incidência da letra no texto
 } ln; // Letra e número de incidência
 
+void Imprime(ln Alfabeto[]);
+
+void Imprime_Ordem_Incidencia(ln Alfabeto[]);
+
+void Inicializa(ln *Alfabeto);
+
 int main(){
 
     char Mensagem[4096];
@@ -24,17 +30,7 @@ int main(){
     // Vetor com 26 posições para armazenar todas as letras do alfabeto e suas respectivas incidências
     ln Alfabeto[26];
 
-    /* Valores ASCII
-    Letra MAIÚSCULA 65 - 90
-    Letra MINÚSCULA 97 - 122
-    */
-    int j = 0;
-    for(int i = 97; i <= 122; i++ && j++){
-        // Inicializa a letra e seu respectivo número de incidência.
-        Alfabeto[j].Letra = i;
-        Alfabeto[j].Num = 0;
-    }
-
+    Inicializa(Alfabeto);
      
     scanf("%[^\n]s", Mensagem);
 
@@ -52,16 +48,53 @@ int main(){
         // printf("%c - %d ", Alfabeto[Numero-97].Letra, Alfabeto[Numero-97].Num);
     }
 
-    j = 0;
-    while(j < 26){
-        printf("%c - %d\n", Alfabeto[j].Letra, Alfabeto[j].Num);
-        j++;
-    }
-    printf("\n");
+    Imprime(Alfabeto);
 
+    Imprime_Ordem_Incidencia(Alfabeto);
 
     return 0;
 
 }
 
+void Inicializa(ln *Alfabeto){
+    /* Valores ASCII
+    Letra MAIÚSCULA 65 - 90
+    Letra MINÚSCULA 97 - 122
+    */
+    int j = 0;
+    for(int i = 97; i <= 122; i++ && j++){
+        // Inicializa a letra e seu respectivo número de incidência.
+        Alfabeto[j].Letra = i;
+        Alfabeto[j].Num = 0;
+    }
+}
+
+void Imprime(ln Alfabeto[]){
+    int j = 0;
+    while(j < 26){
+        printf("%c - %d\n", Alfabeto[j].Letra, Alfabeto[j].Num);
+        j++;
+    }
+    printf("\n");
+}
+
+void Imprime_Ordem_Incidencia(ln Alfabeto[]){
+
+    ln tAlfabeto[26];
+    for(int i = 0; i < 26; i++){
+        tAlfabeto[i].Letra = Alfabeto[i].Letra;
+        tAlfabeto[i].Num = Alfabeto[i].Num; 
+    }
+
+    for(int i = 26; i >= 0; i--){
+        for(int j = 1; j < i; j++){
+            if(tAlfabeto[j-1].Num < tAlfabeto[j].Num){
+                ln Auxiliar = tAlfabeto[j-1];
+                tAlfabeto[j-1] = tAlfabeto[j];
+                tAlfabeto[j] = Auxiliar;
+            }
+        }
+    }
+    Imprime(tAlfabeto);
+}
 
